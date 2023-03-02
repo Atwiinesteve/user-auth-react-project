283a313c0cec8d563c86342b3b7c700b1817cd78
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-02 11:09:43 +0000

    added firebase configs

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+// Importing firebase modules
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyD-OkfzE3ikUVSe7vNBzc9xYplkNseNprc",
+	authDomain: "react-user-auth-84bce.firebaseapp.com",
+	projectId: "react-user-auth-84bce",
+	storageBucket: "react-user-auth-84bce.appspot.com",
+	messagingSenderId: "1006084155999",
+	appId: "1:1006084155999:web:fdaf81672b2b4fb57709ea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
